47c01eb6766d1d8ce9125e7c46e2d9d3175d36aa
mlhaycck <aycckmlh@gmail.com>
2020-04-25 00:13:57 +0300

    ilk gelistirme yapildi

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import melih from './melih.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={melih} className="App-logo" alt="logo" />
+        <p>
+          Melih <code>src/App.js</code> Aycicek.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
